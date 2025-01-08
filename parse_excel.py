9fe0128477fe6cb8e0657f78225331ebe2eb156e
Helmi Tazkia <helmitazkia85@gmail.com>
2025-01-08 13:14:17 +0700

    Feat : Import  and parse for pyton

--- parse_excel.py
+++ parse_excel.py
@@ -35,7 +35,7 @@ def import_text_to_excel(input_file, output_file):
 
 # Example usage
 if __name__ == "__main__":
-    input_file = "tabel_m_customer.txt"
+    input_file = "parse_customer.txt"
     output_file = "customer_data.xlsx"
     
     import_text_to_excel(input_file, output_file)
